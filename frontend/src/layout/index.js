85cf7dc4745dc87aa0f75cd26f5e196e78789195
rafaelbok <108283975+rafaelbok@users.noreply.github.com>
2023-06-17 13:39:31 -0300

    Troca de nome Press

--- frontend/src/layout/index.js
+++ frontend/src/layout/index.js
@@ -247,7 +247,7 @@ const LoggedInLayout = ({ children }) => {
             noWrap
             className={classes.title}
           >
-            {i18n.t("mainDrawer.appBar.message.hi")} {user.name}, {i18n.t("mainDrawer.appBar.message.text")} {system.name || "Press Ticket"}
+            {i18n.t("mainDrawer.appBar.message.hi")} {user.name}, {i18n.t("mainDrawer.appBar.message.text")} {system.name || "WaSap"}
           </Typography>
           {user.id && <NotificationsPopOver />}
 
